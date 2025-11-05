5015804ca7d6ccaa96a0a17c11a70674fe4190f8
Srivats Jayaram <srivats666@gmail.com>
2025-11-05 14:00:59 -0800

    Google, Git and email/password sign-in and sign-up.

--- services/frontend/app/page.tsx
+++ services/frontend/app/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view"
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Button>Click Me</Button>
-    </div>
-  );
-}
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  return <HomeView />;
+};
+
+export default Page;
