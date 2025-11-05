97006d2d6d4fc5abf153c8aac43f531f611bf32c
Srivats Jayaram <srivats666@gmail.com>
2025-11-05 09:16:57 -0800

    Setup nextjs app with shadcn and tailwind.

---
+++ services/frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
